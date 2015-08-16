41661b859090ee86c0d17f9cfcb0bafe9fa9addc
Maxim Kholodnyak <mvkholodnyak@gmail.com>
2015-08-16 15:02:32 +0300

    Добавил browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
     path = require('path'),
+    browserify = require('browserify'),
+    watchify = require('watchify'),
+    babelify = require("babelify"),
+    sourceStream = require('vinyl-source-stream'),
     $ = require('gulp-load-plugins')(),
     browserSync = require('browser-sync').create(),
     liveReload = browserSync.reload;
@@ -10,9 +14,20 @@ var config = {
     htmlSrc: 'index.html'
 };
 
+var handleError = function () {
+    var args = Array.prototype.slice.call(arguments);
+
+    $.notify.onError({
+        title: "Compile Error",
+        message: "<%= error.message %>"
+    }).apply(this, args);
+
+    this.emit('end');
+};
+
 gulp.task('default', ['start-livereload-server', 'build-files']);
 
-gulp.task('build-files', ['html', 'css', 'image', 'font']);
+gulp.task('build-files', ['html', 'css', 'browserify', 'image', 'font']);
 
 gulp.task('start-livereload-server', function () {
     browserSync.init({
@@ -52,4 +67,42 @@ gulp.task('image', function () {
     gulp.src(config.appPath + '/images/**/*.{png,svg,jpg,jpeg}')
         .pipe(gulp.dest(config.outPath + config.appPath + 'images'))
         .pipe(liveReload({stream: true}));
+});
+
+gulp.task('browserify', function () {
+    var srcPath = path.join(__dirname, config.appPath + 'js/');
+    var browserifyFiles = function (srcArray) {
+        return srcArray.map(function (sourceStr) {
+            // Удаляю расширение .js
+            var filename = sourceStr.split('/').pop().slice(0, -3);
+
+            console.log(srcPath);
+            console.log(filename);
+            var bundler = watchify(browserify(sourceStr, {
+                debug: true,
+                cache: {},
+                packageCache: {}
+            })
+                .transform(babelify));
+
+            function rebundle() {
+                bundler.bundle().on('error', handleError)
+                    .pipe(sourceStream(filename + ".Bundle.js"))
+                    .pipe($.streamify($.uglify().on('error', handleError)))
+                    .pipe(gulp.dest(config.outPath + config.appPath + 'js/Bundle'))
+                    .pipe(liveReload({stream: true}));
+            }
+
+            bundler.on('update', function () {
+                console.log('-> bundling...');
+                rebundle();
+            });
+            rebundle();
+        });
+    };
+
+    var bundleFiles = ['index.js'];
+    browserifyFiles(bundleFiles.map(function (filename) {
+        return srcPath + filename;
+    }));
 });
