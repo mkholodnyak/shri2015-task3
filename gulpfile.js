c347d385b4ba307d0ac055e3ab66c4ee6493d28e
Maxim Kholodnyak <mvkholodnyak@gmail.com>
2015-08-16 12:24:23 +0300

    Добавил разметку плеера и начальные файлы

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    path = require('path'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create(),
+    liveReload = browserSync.reload;
+
+var config = {
+    outPath: 'public/',
+    appPath: 'app/',
+    htmlSrc: 'index.html'
+};
+
+gulp.task('default', ['start-livereload-server', 'build-files']);
+
+gulp.task('build-files', ['html', 'css', 'image']);
+
+gulp.task('start-livereload-server', function () {
+    browserSync.init({
+        server: config.outPath
+    });
+
+    gulp.watch('*.html', ['html']);
+    gulp.watch(config.appPath + '/css/**/*.scss', ['css']);
+});
+
+
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe($.rename('index.html'))
+        .pipe($.minifyHtml())
+        .pipe(gulp.dest(config.outPath))
+        .pipe(liveReload({stream: true}));
+});
+
+gulp.task('css', function () {
+    gulp.src(config.appPath + '/css/**/*.scss')
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe($.autoprefixer())
+        .pipe($.minifyCss())
+        .pipe(gulp.dest(config.outPath + config.appPath + 'css'))
+        .pipe(liveReload({stream: true}));
+});
+
+gulp.task('image', function () {
+    gulp.src(config.appPath + '/images/**/*.{png,svg,jpg,jpeg}')
+        .pipe(gulp.dest(config.outPath + config.appPath + 'images'))
+        .pipe(liveReload({stream: true}));
+});
